76ec7d73bb54ea9b3786505a9eefd00686ee328f
Olga Kharitonova <113110497+husaini1986@users.noreply.github.com>
2023-05-26 17:12:05 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,136 @@
+
+let command = document.querySelector ('.command__list');
+
+let command2004 = document.querySelector('.command__2004');
+let command2005 = document.querySelector('.command__2005');
+let command2006 = document.querySelector('.command__2006');
+let command2007 = document.querySelector('.command__2007');
+let command2008 = document.querySelector('.command__2008');
+let command2009 = document.querySelector('.command__2009');
+let command2010 = document.querySelector('.command__2010');
+let command2011 = document.querySelector('.command__2011');
+
+let choose2004 =document.querySelector ('.c2004');
+let choose2005 =document.querySelector ('.c2005');
+let choose2006 =document.querySelector ('.c2006');
+let choose2007 =document.querySelector ('.c2007');
+let choose2008 =document.querySelector ('.c2008');
+let choose2009 =document.querySelector ('.c2009');
+let choose2010 =document.querySelector ('.c2010');
+let choose2011 =document.querySelector ('.c2011');
+
+
+let b2004=document.querySelector ('.b2004');
+let b2005=document.querySelector ('.b2005');
+let b2006=document.querySelector ('.b2006');
+let b2007=document.querySelector ('.b2007');
+let b2008=document.querySelector ('.b2008');
+let b2009=document.querySelector ('.b2009');
+let b2010=document.querySelector ('.b2010');
+let b2011=document.querySelector ('.b2011');
+
+// 2004
+choose2004.addEventListener('click', function()
+{
+    command.classList.add('hidn');
+    command2004.classList.remove('hidn');
+});
+b2004.addEventListener('click', function()
+{
+    command.classList.remove('hidn');
+    command2004.classList.add('hidn')
+}
+);
+
+// 2005
+
+choose2005.addEventListener('click', function()
+{
+    command.classList.add('hidn');
+    command2005.classList.remove('hidn');
+});
+
+b2005.addEventListener('click', function()
+{
+    command.classList.remove('hidn');
+    command2005.classList.add('hidn');
+}
+);
+// 2006
+choose2006.addEventListener('click', function()
+{
+    command.classList.add('hidn');
+    command2006.classList.remove('hidn');
+});
+
+b2006.addEventListener('click', function()
+{
+    command.classList.remove('hidn');
+    command2006.classList.add('hidn');
+}
+);
+// 2007
+choose2007.addEventListener('click', function()
+{
+    command.classList.add('hidn');
+    command2007.classList.remove('hidn');
+});
+b2007.addEventListener('click', function()
+{
+    command.classList.remove('hidn');
+    command2007.classList.add('hidn');
+}
+);
+// 2008
+choose2008.addEventListener('click', function()
+{
+    command.classList.add('hidn');
+    command2008.classList.remove('hidn');
+});
+
+b2008.addEventListener('click', function()
+{
+    command.classList.remove('hidn');
+    command2008.classList.add('hidn');
+}
+);
+// 2009
+choose2009.addEventListener('click', function()
+{
+    command.classList.add('hidn');
+    command2009.classList.remove('hidn');
+});
+
+b2009.addEventListener('click', function()
+{
+    command.classList.remove('hidn');
+    command2009.classList.add('hidn');
+}
+);
+// 2010
+choose2010.addEventListener('click', function()
+{
+    command.classList.add('hidn');
+    command2010.classList.remove('hidn');
+});
+
+b2010.addEventListener('click', function()
+{
+    command.classList.remove('hidn');
+    command2010.classList.add('hidn');
+}
+);
+// 2011
+choose2011.addEventListener('click', function()
+{
+    command.classList.add('hidn');
+    command2011.classList.remove('hidn');
+});
+
+b2011.addEventListener('click', function()
+{
+    command.classList.remove('hidn');
+    command2011.classList.add('hidn');
+}
+);
+
